a6693cdb0e1cf61f9fa6aef0d450318d2089cd61
saigsf <2270029397@qq.com>
2017-11-05 21:33:00 +0800

    11

---
+++ toutiao/src/components/NavHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+  name: "NavHeader",
+  data:function(){
+      return{
+          msg:"sa"
+      }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
